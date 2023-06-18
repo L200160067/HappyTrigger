30f5b18e9b2a8b6b0cfc6219f56ca568a1c8b187
Bastiar <bastiarramadan76@gmail.com>
2023-06-18 14:38:00 +0700

    memperbaiki movement dan bug tidak bisa lompat

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,25 +7,20 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2D;
     [SerializeField]
-    private bool isJumping;
+    private bool isJumping, isGrounded;
     [SerializeField]
-    private bool isGrounded;
+    private float jumpForce, moveSpeed;
     [SerializeField]
-    private float jumpForce;
-    [SerializeField]
-    private float moveSpeed;
-    [SerializeField]
-    private int score; 
-    [SerializeField]
-    private int health;
+    private int score, health;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        isJumping = false;
-        isGrounded = true;
-        jumpForce = 6f;
+        /*Change the gravity scale*/
+        rb2D.gravityScale = 3f;
+
+        jumpForce = 15f;
         moveSpeed = 5f;
         score = 0;
         health = 3;
@@ -37,21 +32,40 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         rb2D.velocity = new Vector2(moveHorizontal * moveSpeed, rb2D.velocity.y);
 
+        if (moveHorizontal > 0)
+        {
+            transform.localScale = new Vector2(2, 2);
+        }
+        else if (moveHorizontal < 0)
+        {
+            transform.localScale = new Vector2(-2, 2);
+        }
+
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            isJumping = true;
-            isGrounded = false;
+            // isJumping = true;
+            // isGrounded = false;
         }
+
+        isJumping = rb2D.velocity.y > 0 && !isGrounded;
+        // if (rb2D.velocity.y > 0)
+        // {
+        //     isJumping = true;
+        // }
+        // else
+        // {
+        //     isJumping = false;
+        // }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isJumping = false;
-            isGrounded = true;
-        }
+        // if (collision.gameObject.CompareTag("Ground"))
+        // {
+        //     isJumping = false;
+        //     isGrounded = true;
+        // }
 
         if (collision.gameObject.CompareTag("Enemy"))
         {
@@ -63,14 +77,25 @@ public class PlayerController : MonoBehaviour
             CollectItem(collision.gameObject);
         }
     }
-
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
+        // if (collision.gameObject.CompareTag("Ground"))
+        // {
+        //     isGrounded = false;
+        // }
     }
+
+    /*Ground Check*/
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        isGrounded = other.gameObject.CompareTag("Ground");
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        isGrounded = !other.gameObject.CompareTag("Ground");
+    }
+
+
 
     private void CollectItem(GameObject item)
     {
@@ -100,5 +125,5 @@ public class PlayerController : MonoBehaviour
         Debug.Log("Score: " + score);
     }
 
-   
+
 }
