b9be9c9fe6a8c9a6d60cbd7dd89e760a80fff2ae
Arifinsetya <setyanugroho58@gmail.com>
2023-07-15 18:43:15 +0700

    BGM
    UI quest, BGM

--- Assets/Script/Enemy/Enemy.cs
+++ Assets/Script/Enemy/Enemy.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+<<<<<<< Updated upstream
 
     public int damage = 1, health = 10;
+=======
+    public int health = 20, damage = 1, knockbackPower, speed = 4;
+    public float attackCD = 2f, rangeToAtk = 3.5f;
+    [NonSerialized]
+    public bool playerDetected, isAttacking;
+    bool isCD = false;
+    float Cooldown = 0;
+    Transform target;
+    Animator anim;
+>>>>>>> Stashed changes
 
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -22,4 +33,70 @@ public class Enemy : MonoBehaviour
     {
 
     }
+<<<<<<< Updated upstream
+=======
+    IEnumerator AttackCooldown()
+    {
+        Cooldown = Cooldown - Time.deltaTime;
+        yield return new WaitUntil(() => isCD == false);
+        Cooldown = 0;
+    }
+
+    void Move()
+    {
+        float playerDistanceX = Vector2.Distance(new Vector2(transform.position.x, 0), new Vector2(target.position.x, 0));
+        float playerDistanceY = Vector2.Distance(new Vector2(0, transform.position.y), new Vector2(0, target.position.y));
+        if (FindObjectOfType<PlayerHealth>().isAlive)
+        {
+            if (playerDetected && playerDistanceX >= rangeToAtk)
+            {
+                anim.SetBool("isMoving", true);
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), speed * Time.deltaTime);
+                if (target.position.x > transform.position.x)
+                {
+                    transform.localScale = new Vector2(transform.localScale.x < 0 ? transform.localScale.x : -transform.localScale.x, transform.localScale.y);
+                }
+                else if (target.position.x < transform.position.x)
+                {
+                    transform.localScale = new Vector2(transform.localScale.x > 0 ? transform.localScale.x : -transform.localScale.x, transform.localScale.y);
+                }
+            }
+            else if (playerDistanceX < rangeToAtk /*&& playerDistanceY < 3f*/)
+            {
+                Attack();
+                anim.SetBool("isMoving", false);
+                Debug.Log("in atk range");
+            }
+            else
+            {
+                anim.SetBool("isMoving", false);
+            }
+            if (Vector2.Distance(transform.position, target.position) > 20 && playerDetected)
+            {
+                playerDetected = false;
+            }
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(int damage, float knockback = 0)
+    {
+        health -= damage;
+        // anim.SetTrigger("hurt");
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(knockback, 4f), ForceMode2D.Impulse);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && other.gameObject.GetComponent<PlayerHealth>().isAlive)
+        {
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage, knockbackPower);
+        }
+    }
+
+>>>>>>> Stashed changes
 }
--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -34,7 +34,7 @@ public class PlayerController : MonoBehaviour
             Jump();
             Attack();
         }
-        if (transform.position.y <= -35f)
+        if (transform.position.y <= -100f)
         {
             playerHealth.health = 0;
         }
